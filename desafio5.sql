1e8ba50b663f27ae23e4c786fc2878e2d8e69937
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-07-25 17:48:06 -0400

    Fix: Atualiza o id incorreto REQ5

--- desafio5.sql
+++ desafio5.sql
@@ -3,7 +3,7 @@ m.titulo AS `cancao`,
 COUNT(*) AS `reproducoes`
 FROM SpotifyClone.musica m
 INNER JOIN SpotifyClone.historico_de_reproducao hr
-ON m.id = hr.usuario_id
+ON m.id = hr.musica_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2;
