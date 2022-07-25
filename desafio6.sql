62365d48c6c47ddbcae59981aa3ee2e1c1857920
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-07-25 17:42:05 -0400

    Fix: Atualiza o id do REQ6

--- desafio6.sql
+++ desafio6.sql
@@ -5,4 +5,4 @@ ROUND(AVG(p.valor), 2) AS `faturamento_medio`,
 SUM(p.valor) AS `faturamento_total`
 FROM SpotifyClone.plano p
 INNER JOIN SpotifyClone.usuario u
-ON p.id = u.id;
+ON p.id = u.plano_id;
