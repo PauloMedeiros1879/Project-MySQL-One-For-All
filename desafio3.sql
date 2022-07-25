831549d5b6adfed2a95e9b89c32b586c48fee607
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-07-25 15:45:15 -0400

    Fix: Atualizando as informações do desafio1, e corrigindo o erro do desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,11 @@
 SELECT 
-  u.usuario AS `usuario`, 
-  COUNT(*) AS 'qtde_musicas_ouvidas',
-    ROUND(SUM(tm.duracao)/60, 2) AS 'total_minutos'
-FROM SpotifyClone.historico_reproducao hr
-INNER JOIN SpotifyClone.usuario u
-ON hr.usuario_id = u.usuario_id
-INNER JOIN SpotifyClone.cancao c
-ON hr.cancao_id = c.cancao_id
-INNER JOIN SpotifyClone.cancao tm
-ON tm.id = hr.musica_id
-ORDER BY `usuario`, `qtde_musicas_ouvidas`, `total_minutos`;
+	u.nome AS 'usuario',
+    COUNT(*)AS 'qtde_musicas_ouvidas',
+    ROUND(SUM(m.duracao)/60, 2) AS 'total_minutos'
+FROM SpotifyClone.usuario u
+INNER JOIN SpotifyClone.historico_de_reproducao h
+ON u.id = h.usuario_id
+INNER JOIN SpotifyClone.musica m
+ON m.id = h.musica_id
+GROUP BY u.id
+ORDER BY u.nome;
