e02191345e3d32957a33081cd937874f3318aed7
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-07-25 14:44:46 -0400

    Feat: Cria uma query que deverá ter apenas 3 colunas - REQ3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario AS `usuario`, c.cancao AS `qtde_musicas_ouvidas`, c.cancao AS `total_minutos`
+FROM SpotifyClone.historico_reproducao hr
+INNER JOIN SpotifyClone.usuario u
+ON hr.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.cancao c
+ON hr.cancao_id = c.cancao_id
+ORDER BY `usuario`, `qtde_musicas_ouvidas`, `total_minutos`;
